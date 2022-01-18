b391754d0f4d8644ef9e5371258d3659e3fada6d
Finn <flabe@physnet.uni-hamburg.de>
2022-01-18 18:19:01 +0100

    First preprocessing module

---
+++ src/UHH2L1toolsModule.cxx
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <memory>
+
+#include "UHH2/core/include/AnalysisModule.h"
+#include "UHH2/core/include/Event.h"
+
+using namespace std;
+using namespace uhh2;
+
+namespace uhh2examples {
+
+  /** \brief UHH module to prepare conversion to DF for L1 triggers
+   *
+   */
+  class UHH2L1toolsModule: public AnalysisModule {
+  public:
+
+    explicit UHH2L1toolsModule(Context & ctx);
+    virtual bool process(Event & event) override;
+
+  private:
+
+    // handle object
+    uhh2::Event::Handle<double> h_evt_weight;
+
+  };
+
+
+  UHH2L1toolsModule::UHH2L1toolsModule(Context & ctx){
+
+    std::cout << "Preparing run..."  << std::endl;
+
+    // defining the handle that will store the event weights
+    h_evt_weight = ctx.declare_event_output<double>("evt_weight");
+
+    std::cout << "end prepare run..."  << std::endl;
+
+
+  }
+
+
+  bool UHH2L1toolsModule::process(Event & event) {
+
+    std::cout << "processing event"  << std::endl;
+
+    // outputting the weight to the AnalysisTree
+    event.set(h_evt_weight, event.weight);
+
+    return true;
+  }
+
+  UHH2_REGISTER_ANALYSIS_MODULE(UHH2L1toolsModule)
+
+}
